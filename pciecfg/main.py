a73b705e35624cca8bd2e8da7704149115d815cb
Gavyn <engguopeng@buaa.edu.cn>
2025-06-08 20:55:39 +0800

    feat(parser): implement PCIeRegisterParser for enhanced register access and fuzzy matching
    - Introduced PCIeRegisterParser class to manage PCIe register structures and provide fuzzy matching for register and field names.
    - Refactored build_all_structures function into the new class, improving organization and encapsulation of functionality.
    - Updated main execution logic to utilize the new parser for accessing register details, enhancing user interaction with configuration space data.
    - Removed obsolete text.py file as its functionality is now integrated into the parser.
    
    Signed-off-by: Gavyn <engguopeng@buaa.edu.cn>

--- pciecfg/main.py
+++ pciecfg/main.py
@@ -1,32 +1,57 @@
 import sys
 import os
+from thefuzz import fuzz
+from thefuzz import process
+
 # add current directory to sys.path
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from pci import build_pci_structures_from_json
 from pcie import build_pcie_structures_from_json
 from extend import build_pcie_extended_structures_from_json
 
+class PCIeRegisterParser:
+    def __init__(self, config_space):
 
-def build_all_structures(config_space: bytes):
-    all_structures = []
-    structures = build_pci_structures_from_json(config_space)
-    all_structures.extend(structures)
-    structures = build_pcie_structures_from_json(config_space)
-    all_structures.extend(structures)
-    structures = build_pcie_extended_structures_from_json(config_space)
-    all_structures.extend(structures)
+        self.all_structures_map = {}
+        self.all_structures = self.build_all_structures(config_space)
 
-    return all_structures
+        for reg_struct in self.all_structures:
+            for reg in reg_struct.registers:
+                self.all_structures_map[reg.name] = reg
+                for field in reg.fields:
+                    self.all_structures_map[field.name] = field
+
+    def __getitem__(self, key):
+        correct_key, score = process.extractOne(key, self.all_structures_map.keys())
+        if score > 80:
+            print(f'Use {key}, You mean {correct_key}')
+        else:
+            print(f'Use {key}, May be you mean {correct_key}')
+        return self.all_structures_map[correct_key]
+   
+  
+
+    def build_all_structures(self, config_space: bytes):
+        all_structures = []
+        structures = build_pci_structures_from_json(config_space)
+        all_structures.extend(structures)
+        structures = build_pcie_structures_from_json(config_space)
+        all_structures.extend(structures)
+        structures = build_pcie_extended_structures_from_json(config_space)
+        all_structures.extend(structures)
+
+        return all_structures
 
 
 
 if __name__ == "__main__":
     with open("pciecfg/mock_data/config_space.bin", "rb") as f:
         config_space = bytearray(f.read())
-        all_structures = build_all_structures(config_space)
-        for reg_struct in all_structures:
-            print(reg_struct.name)
-            for reg in reg_struct.registers:
-                for field in reg.fields:
-                    print(field.name, reg[field.name])
+        all_structures = PCIeRegisterParser(config_space)
+        print(all_structures['vendor_id'])
+        # for reg_struct in all_structures:
+        #     print(reg_struct.name)
+        #     for reg in reg_struct.registers:
+        #         for field in reg.fields:
+        #             print(field.name, reg[field.name])
             
